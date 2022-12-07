1776fa3602cae1c5755dbaa221adffa19c11a892
blap <baudouinlapeze9@gmail.com>
2022-12-07 23:40:59 +0100

    Fix : Enemy FOW does not appear ?

--- GardenWarProject/Assets/Scripts/AI/SpawnAIs.cs
+++ GardenWarProject/Assets/Scripts/AI/SpawnAIs.cs
@@ -24,34 +24,43 @@ public class SpawnAIs : MonoBehaviourPun
 
     IEnumerator SpawnerMinions()
     {
-        while (!StopSpawn)
+        for (int i = 0; i < nb; i++)
         {
-            for (int i = 0; i < nb; i++)
-            {
-                //Entity entity = PoolNetworkManager.Instance.PoolInstantiate(minion, SpawnPoints[Team - 1].position, Quaternion.identity);
-                Entity BlueEntity = PhotonNetwork.Instantiate("Minion", SpawnPoints[0].position, Quaternion.identity)
-                    .GetComponent<Entity>();
-                Entity RedEntity = PhotonNetwork.Instantiate("Minion", SpawnPoints[1].position, Quaternion.identity)
-                    .GetComponent<Entity>();
-                yield return new WaitForSeconds(0.1f);
-                photonView.RPC("SyncDataMinions", RpcTarget.All, BlueEntity.photonView.ViewID,
-                    RedEntity.photonView.ViewID);
-                yield return new WaitForSeconds(0.1f);
-                BlueEntity.team = Enums.Team.Team1;
-                RedEntity.team = Enums.Team.Team2;
-                MyAIBT BlueBT = BlueEntity.GetComponent<MyAIBT>();
-                MyAIBT RedBT = RedEntity.GetComponent<MyAIBT>();
-                BlueBT.enabled = true;
-                RedBT.enabled = true;
-                BlueBT.waypoints = waypointsTeamBlue;
-                RedBT.waypoints = waypointsTeamRed;
-                BlueBT.OnStart();
-                RedBT.OnStart();
-                yield return new WaitForSeconds(delayBetweenSpawn);
-            }
-
-            yield return new WaitForSeconds(DelayBetweenWaves);
+            //Entity entity = PoolNetworkManager.Instance.PoolInstantiate(minion, SpawnPoints[Team - 1].position, Quaternion.identity);
+            Entity BlueEntity = PhotonNetwork.Instantiate("Minion", SpawnPoints[0].position, Quaternion.identity)
+                .GetComponent<Entity>();
+            Entity RedEntity = PhotonNetwork.Instantiate("Minion", SpawnPoints[1].position, Quaternion.identity)
+                .GetComponent<Entity>();
+            yield return new WaitForSeconds(0.1f);
+            photonView.RPC("SyncDataMinions", RpcTarget.All, BlueEntity.photonView.ViewID,
+                RedEntity.photonView.ViewID);
+            yield return new WaitForSeconds(0.1f);
+            BlueEntity.team = Enums.Team.Team1;
+            RedEntity.team = Enums.Team.Team2;
+            MyAIBT BlueBT = BlueEntity.GetComponent<MyAIBT>();
+            MyAIBT RedBT = RedEntity.GetComponent<MyAIBT>();
+            BlueBT.enabled = true;
+            RedBT.enabled = true;
+            BlueBT.waypoints = waypointsTeamBlue;
+            RedBT.waypoints = waypointsTeamRed;
+            BlueBT.OnStart();
+            RedBT.OnStart();
+            yield return new WaitForSeconds(delayBetweenSpawn);
         }
+        yield return new WaitForSeconds(DelayBetweenWaves);
+        StartCoroutine(SpawnerMinions());
+    }
+
+    [PunRPC]
+    public void SyncDataMinions(int entityIDBlue, int entityIDRed)
+    {
+        Entity BlueEntity = EntityCollectionManager.GetEntityByIndex(entityIDBlue);
+        Entity RedEntity = EntityCollectionManager.GetEntityByIndex(entityIDRed);
+
+        BlueEntity.team = Enums.Team.Team1;
+        RedEntity.team = Enums.Team.Team2;
+        BlueEntity.ChangeColor();
+        RedEntity.ChangeColor();
     }
 
     IEnumerator SpawnerTowers()
@@ -60,6 +69,7 @@ public class SpawnAIs : MonoBehaviourPun
             .GetComponent<Entity>();
         Entity BlueTower = PhotonNetwork.Instantiate("NewTower", SpawnerTower[1].position, Quaternion.identity)
             .GetComponent<Entity>();
+
         BlueTower.team = Enums.Team.Team1;
         RedTower.team = Enums.Team.Team2;
         yield return new WaitForSeconds(0.5f);
@@ -69,15 +79,16 @@ public class SpawnAIs : MonoBehaviourPun
             new int[] { RedTower.photonView.ViewID, BlueTower.photonView.ViewID });
     }
 
-
-    // Start is called before the first frame update
-    void Start()
+    [PunRPC]
+    public void SyncDataTower(int[] IDS)
     {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            photonView.RPC("SpawnTowers", RpcTarget.MasterClient);
-            photonView.RPC("SpawnMinions", RpcTarget.MasterClient);
-        }
+        Entity entity = EntityCollectionManager.GetEntityByIndex(IDS[0]);
+        entity.team = Enums.Team.Team2;
+        entity.ChangeColor();
+
+        entity = EntityCollectionManager.GetEntityByIndex(IDS[1]);
+        entity.team = Enums.Team.Team1;
+        entity.ChangeColor();
     }
 
     [PunRPC]
@@ -92,28 +103,12 @@ public class SpawnAIs : MonoBehaviourPun
         StartCoroutine(SpawnerTowers());
     }
 
-
-    [PunRPC]
-    public void SyncDataMinions(int entityIDBlue, int entityIDRed)
+    void Start()
     {
-        Entity BlueEntity = EntityCollectionManager.GetEntityByIndex(entityIDBlue);
-        Entity RedEntity = EntityCollectionManager.GetEntityByIndex(entityIDRed);
-
-        BlueEntity.team = Enums.Team.Team1;
-        RedEntity.team = Enums.Team.Team2;
-        BlueEntity.ChangeColor();
-        RedEntity.ChangeColor();
-    }
-
-    [PunRPC]
-    public void SyncDataTower(int[] IDS)
-    {
-        Entity entity = EntityCollectionManager.GetEntityByIndex(IDS[0]);
-        entity.team = Enums.Team.Team2;
-        entity.ChangeColor();
-
-        entity = EntityCollectionManager.GetEntityByIndex(IDS[1]);
-        entity.team = Enums.Team.Team1;
-        entity.ChangeColor();
+        if (PhotonNetwork.IsMasterClient)
+        {
+            photonView.RPC("SpawnTowers", RpcTarget.MasterClient);
+            photonView.RPC("SpawnMinions", RpcTarget.MasterClient);
+        }
     }
 }
