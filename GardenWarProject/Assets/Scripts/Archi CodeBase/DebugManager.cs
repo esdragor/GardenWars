ec5815f6612e017a759731dda450038b1a0694f9
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-12-08 05:44:35 +0100

    FPS and TPS display

--- GardenWarProject/Assets/Scripts/Archi CodeBase/DebugManager.cs
+++ GardenWarProject/Assets/Scripts/Archi CodeBase/DebugManager.cs
@@ -7,30 +7,53 @@ using UnityEngine;
 public class DebugManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI clientDataText;
+    [SerializeField] private TextMeshProUGUI frameDataText;
+    private GameStateMachine gsm => GameStateMachine.Instance;
+    
+    private double timer;
+    private int frameCount;
+    private int tickCount;
 
     private void Start()
     {
         var id = PhotonNetwork.LocalPlayer.ActorNumber;
         clientDataText.text = $"Client {id} / {GameStateMachine.Instance.GetPlayerTeam()}";
+        gsm.OnTick += (() => tickCount++);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        frameCount++;
+        if(timer < 1) return;
+        timer = 0;
+        UpdateFrameData();
+    }
+
+    private void UpdateFrameData()
+    {
+        frameDataText.text = $"FPS : {frameCount} \nTPS : {tickCount}";
+        frameCount = 0;
+        tickCount = 0;
     }
 
     public void OnStartInGameState()
     {
-        GameStateMachine.Instance.SwitchState(2);
+        gsm.SwitchState(2);
     }
     
     public void OnTeamWins(int index)
     {
-        GameStateMachine.Instance.winner = index == 0 ? Enums.Team.Team1 : Enums.Team.Team2;
+        gsm.winner = index == 0 ? Enums.Team.Team1 : Enums.Team.Team2;
     }
 
     public void OnDieButtonClick()
     {
-        GameStateMachine.Instance.GetPlayerChampion().RequestDie();
+        gsm.GetPlayerChampion().RequestDie();
     }
 
     public void OnDamageButtonClick()
     {
-        GameStateMachine.Instance.GetPlayerChampion().DecreaseCurrentHpRPC(2);
+        gsm.GetPlayerChampion().DecreaseCurrentHpRPC(2);
     }
 }
