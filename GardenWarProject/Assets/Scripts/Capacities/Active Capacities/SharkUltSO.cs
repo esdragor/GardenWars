a3174a44d3d1224c57172ed357ca28658ecb2a2b
blap <baudouinlapeze9@gmail.com>
2023-01-15 17:39:11 +0100

    try

--- GardenWarProject/Assets/Scripts/Capacities/Active Capacities/SharkUltSO.cs
+++ GardenWarProject/Assets/Scripts/Capacities/Active Capacities/SharkUltSO.cs
@@ -41,6 +41,7 @@ namespace Entities.Capacities
 
         protected override bool AdditionalCastConditions(int targetsEntityIndexes, Vector3 targetPositions)
         {
+        if(true)
             return true;
         }
 
