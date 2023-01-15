aeafffb8531e67a7245d664b54afe1ccf817fef6
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2023-01-15 02:36:01 +0100

    Active on cooldown shows correctly

--- GardenWarProject/Assets/Scripts/UI/InGame/UIActiveIcon.cs
+++ GardenWarProject/Assets/Scripts/UI/InGame/UIActiveIcon.cs
@@ -1,4 +1,5 @@
 using Entities.Capacities;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,17 +7,41 @@ public class UIActiveIcon : MonoBehaviour
 {
     [SerializeField] private Image iconImage;
     [SerializeField] private Image cooldownOverlayImage;
+    [SerializeField] private TextMeshProUGUI cooldownOverLayText;
+    
     private ActiveCapacity capacity;
 
     private void Start()
     {
-        cooldownOverlayImage.fillAmount = 0;
+        ResetFill();
+    }
+
+    private void Update()
+    {
+        if(!(capacity is {isOnCooldown: true})) return;
+
+        cooldownOverlayImage.fillAmount = (float)capacity.cooldownTimer / (float)capacity.baseCooldown;
+
+        cooldownOverLayText.text = $"{(int)capacity.cooldownTimer+1}";
     }
 
     public void SetCapacity(ActiveCapacity active)
     {
+        if (capacity != null)
+        {
+            capacity.OnCooldownEnded -= ResetFill;
+        }
+        
         capacity = active;
         iconImage.sprite = capacity.AssociatedActiveCapacitySO().icon;
         cooldownOverlayImage.fillAmount = 0;
+
+        capacity.OnCooldownEnded += ResetFill;
+    }
+
+    private void ResetFill()
+    {
+        cooldownOverlayImage.fillAmount = 0;
+        cooldownOverLayText.text = string.Empty;
     }
 }
