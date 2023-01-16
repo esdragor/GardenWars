d0cb3264806ad2a4ded2f3f0cd248a74366e3f7c
Théo Dalmasso <theo.alec.dalmasso@gmail.com>
2023-01-16 18:27:53 +0100

    Auto stash before rebase of "develop"

--- GardenWarProject/Assets/Art/VFX/Archer/Grab/LineGenerate.cs
+++ GardenWarProject/Assets/Art/VFX/Archer/Grab/LineGenerate.cs
@@ -7,27 +7,46 @@ public class LineGenerate : MonoBehaviour
 {
     public Transform startPoint;
     public Transform endPoint;
+    public int endCapVertices = 5;
+
     public float lineWidth;
-    public Color c1;
-    public Color c2;
+    /*public Color c1;
+    public Color c2;*/
+    public int vertexCount = 2;
+    public Material mat;
     private LineRenderer lineRenderer;
-    
+    public Gradient grad;
+    public bool isHorizontal = false;
+
     void Start()
     {
         lineRenderer = gameObject.AddComponent<LineRenderer>();
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        if(mat != null)
+        {
+            lineRenderer.material = mat;
+        }
+        else lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         lineRenderer.widthMultiplier = lineWidth;
-        lineRenderer.startColor = c1;
-        lineRenderer.endColor = c2;
-        lineRenderer.numCapVertices = 5;
-
+        /*lineRenderer.startColor = c1;
+        lineRenderer.endColor = c2;*/
+        lineRenderer.colorGradient = grad;
+        lineRenderer.textureMode = LineTextureMode.Tile;
+        lineRenderer.numCapVertices = endCapVertices;
+        lineRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        if(isHorizontal) lineRenderer.alignment = LineAlignment.TransformZ;
+        lineRenderer.positionCount = vertexCount;
         
 
     }
     
     void Update()
     {
+        for(int i = 0; i < vertexCount; i++)
+        {
+            lineRenderer.SetPosition(i, Vector3.Lerp(startPoint.position, new Vector3(endPoint.position.x, startPoint.position.y - 0.01f, endPoint.position.z),(float)i/(vertexCount-1)));
+            print((float)i / (vertexCount - 1));
+        }/*
         lineRenderer.SetPosition(0, startPoint.position);
-        lineRenderer.SetPosition(1, endPoint.position);
+        lineRenderer.SetPosition(1, new Vector3(endPoint.position.x, startPoint.position.y-0.01f, endPoint.position.z));*/
     }
 }
