d3669dd7c8c2229e97636935fdcd11ac89e90ce6
ETU\h.ramiandrasoa <h.ramiandrasoa@rubika-edu.com>
2023-01-17 11:09:20 +0100

    Started pinata spawn logic

--- GardenWarProject/Assets/Scripts/AI/SpawnAIs.cs
+++ GardenWarProject/Assets/Scripts/AI/SpawnAIs.cs
@@ -16,28 +16,36 @@ public class SpawnAIs : MonoBehaviourPun
 {
     [HideInInspector] public List<RespawnPinata> PinatasRespawned = new List<RespawnPinata>();
 
-    [Header("Wave Management")] public int minionsPerWave = 1;
+    [Header("Wave Management")]
+    public int minionsPerWave = 1;
     public double timeBetweenMinionSpawn = 0.3;
     public double timeBetweenWaves = 30;
     [SerializeField] private double timeBeforeFirstWave = 5;
     [SerializeField] private List<Transform> SpawnPoints;
-    [Header("Minions")] [SerializeField] private Transform[] waypointsTeamBlue;
+    
+    [Header("Minions")]
+    [SerializeField] private Transform[] waypointsTeamBlue;
     [SerializeField] private Transform[] waypointsTeamRed;
     [SerializeField] private Entity minion;
     [SerializeField] private double timer;
     [SerializeField] private Animator[] animatorsTraps;
     [SerializeField] private Transform[] BasketGoal;
 
-    [Header("Towers")] [SerializeField] private Transform[] towerSpawnPoints;
+    [Header("Towers")]
+    [SerializeField] private Transform[] towerSpawnPoints;
 
-    [Header("Pinata")] [SerializeField] private Transform[] pinataSpawnPoints;
+    [Header("Pinata")]
+    [SerializeField] private Transform[] pinataSpawnPoints;
     [SerializeField] float timeBeforeSpawnPinata = 4;
     [SerializeField] float timeBeforeRespawnPinata = 5;
+    [SerializeField] float timeBetweenPinataLevelUp = 5;
+    
+    private Pinata currentPinata;
+    private Transform previousSpawnPos;
 
     private GameStateMachine gsm => GameStateMachine.Instance;
 
     private readonly List<Entity> towers = new List<Entity>();
-    private readonly List<Entity> Pinatas = new List<Entity>();
     private double timerPinata;
 
 
@@ -83,6 +91,7 @@ public class SpawnAIs : MonoBehaviourPun
         }
 
         timer = timeBetweenWaves - timeBeforeFirstWave;
+        
         gsm.OnTick += SpawnWaves;
         gsm.OnTick += InitPinata;
         gsm.OnTick += RespawnPinata;
@@ -94,6 +103,46 @@ public class SpawnAIs : MonoBehaviourPun
         timerPinata += gsm.increasePerTick;
     }
 
+    #region Pinata
+
+    public void SpawnPinata(Transform tr)
+    {
+        previousSpawnPos = tr;
+        currentPinata = NetworkPoolManager.PoolInstantiate("Pinata", tr.position, Quaternion.identity).GetComponent<Pinata>();
+        currentPinata.SyncInstantiate(Enums.Team.Neutral);
+    }
+
+    private void InitPinata()
+    {
+        if (timerPinata < timeBeforeSpawnPinata) return;
+        
+        gsm.OnTick -= InitPinata;
+        
+        SpawnPinata(pinataSpawnPoints[0]);
+    }
+
+    private Transform NextSpawnPoint()
+    {
+        var tr = pinataSpawnPoints[Random.Range(0, pinataSpawnPoints.Length)];
+        return tr != previousSpawnPos ? tr : NextSpawnPoint();
+    }
+    
+    
+
+    public void RespawnPinata()
+    {
+        if(timerPinata < timeBetweenPinataLevelUp) return;
+        
+        timer = 0;
+        
+        
+
+        // TODO - Level Up Pinata
+    }
+
+    #endregion
+    
+    
     #region Towers
 
     private void InitTowers()
@@ -110,41 +159,7 @@ public class SpawnAIs : MonoBehaviourPun
         towers.Add(blueTower);
         towers.Add(redTower);
     }
-
-    public void SpawnPinata(Transform pos)
-    {
-        var Pinata = NetworkPoolManager.PoolInstantiate("Pinata", pos.position, Quaternion.identity)
-            .GetComponent<Entity>();
-
-
-        Pinatas.Add(Pinata);
-        Pinata.SyncInstantiate(Enums.Team.Neutral);
-    }
-
-    private void InitPinata()
-    {
-        if (timerPinata < timeBeforeSpawnPinata) return;
-        gsm.OnTick -= InitPinata;
-
-        foreach (Transform pos in pinataSpawnPoints)
-        {
-            SpawnPinata(pos);
-        }
-    }
-
-    public void RespawnPinata()
-    {
-        return;
-        for (int i = 0; i < PinatasRespawned.Count; i++)
-        {
-            PinatasRespawned[i].delay += (float) gsm.increasePerTick;
-            if (PinatasRespawned[i].delay >= timeBeforeRespawnPinata)
-            {
-                SpawnPinata(PinatasRespawned[i].pos);
-                PinatasRespawned.RemoveAt(i);
-            }
-        }
-    }
+    
 
     private void SyncTowerData()
     {
