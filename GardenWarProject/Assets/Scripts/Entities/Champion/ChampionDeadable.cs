fc27b8197f44cd209c056e4cae24f63d476d961a
esdragor <64788862+esdragor@users.noreply.github.com>
2023-01-25 12:05:34 +0100

    anim death launch correctly + cant touch dead person

--- GardenWarProject/Assets/Scripts/Entities/Champion/ChampionDeadable.cs
+++ GardenWarProject/Assets/Scripts/Entities/Champion/ChampionDeadable.cs
@@ -102,14 +102,15 @@ namespace Entities.Champion
                 InputManager.PlayerMap.Inventory.Disable();
                 agent.isStopped = true;
                 agent.ResetPath();
-                agent.ResetPath();
-                
+
                 HideMaxRangeIndicator();
             }
             isAlive = false;
-            SetAnimatorTrigger("Death");
+            SetAnimatorBool("IsAlive", false);
             uiTransform.gameObject.SetActive(false);
             FogOfWarManager.Instance.RemoveFOWViewable(this);
+            
+            coll.enabled = false;
 
             OnDieFeedback?.Invoke(killerId);
         }
@@ -144,7 +145,8 @@ namespace Entities.Champion
         [PunRPC]
         public void SyncReviveRPC()
         {
-            transform.position = respawnPosition;
+            agent.enabled = true;
+            agent.Warp(respawnPosition);
             if (photonView.IsMine)
             {
                 InputManager.PlayerMap.Movement.Enable();
@@ -155,11 +157,13 @@ namespace Entities.Champion
                 agent.destination = transform.position;
             }
             isAlive = true;
+            SetAnimatorBool("ISAlive", true);
             FogOfWarManager.Instance.AddFOWViewable(this);
             rotateParent.gameObject.SetActive(true);
             uiTransform.gameObject.SetActive(true);
             OnReviveFeedback?.Invoke();
             SetAnimatorTrigger("Respawn");
+            coll.enabled = true;
         }
 
         private void Revive()
