579f9dfaf50ab32d2c1be726ad74eea977a0654e
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2023-01-25 03:46:10 +0100

    Fountain Heal Fx

--- GardenWarProject/Assets/Scripts/Capacities/Passive Capacities/FountainHealSO.cs
+++ GardenWarProject/Assets/Scripts/Capacities/Passive Capacities/FountainHealSO.cs
@@ -8,6 +8,7 @@ namespace Entities.Capacities
     [CreateAssetMenu(menuName = "Capacity/PassiveCapacitySO/FountainHeal", fileName = "FountainHeal")]
     public class FountainHealSO : PassiveCapacitySO
     {
+        public ParticleSystem fx;
         public float healthPerSecond = 5f;
 
         public override Type AssociatedType()
@@ -19,19 +20,23 @@ namespace Entities.Capacities
     public class FountainHeal : PassiveCapacity
     {
         private FountainHealSO so => (FountainHealSO) AssociatedPassiveCapacitySO();
-        
+
+        private GameObject fxGo;
         private float timer;
         
         protected override void OnAddedEffects(Entity target)
         {
-            timer = 0;
-
-            gsm.OnTick += IncreaseHp;
+            
         }
 
         protected override void OnAddedFeedbackEffects(Entity target)
         {
+            fxGo = LocalPoolManager.PoolInstantiate(so.fx, champion.position, Quaternion.Euler(-90, 0, 0)).gameObject;
+            fxGo.SetActive(champion.isVisible);
             
+            timer = 0;
+
+            gsm.OnTickFeedback += IncreaseHp;
         }
 
         protected override void OnAddedLocalEffects(Entity target)
@@ -41,12 +46,14 @@ namespace Entities.Capacities
 
         protected override void OnRemovedEffects(Entity target)
         {
-            gsm.OnTick -= IncreaseHp;
+            
         }
 
         protected override void OnRemovedFeedbackEffects(Entity target)
         {
+            gsm.OnTickFeedback -= IncreaseHp;
             
+            fxGo.SetActive(false);
         }
 
         protected override void OnRemovedLocalEffects(Entity target)
@@ -57,7 +64,11 @@ namespace Entities.Capacities
         private void IncreaseHp()
         {
             timer += (float)gsm.increasePerTick;
-                
+
+            fxGo.transform.position = champion.position;
+            
+            fxGo.SetActive(champion.isVisible);
+
             if(timer < 1) return;
             timer = 0;
             
